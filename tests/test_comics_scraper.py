59c77aa852dc5afdba6da4bc3ce6b6b2b52b651b
Alessandro De Angelis <alessandrodea22@gmail.com>
2017-05-23 11:08:26 +0200

    Added test

---
+++ tests/test_comics_scraper.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+
+def test_comics_scraper():
+    os.chdir("comics")
+    subprocess.call([
+        'python', 'downloader.py', '-u', 'http://readcomiconline.to/Comic/X-Factor-1986/Issue-12?id=40621&readType=1'
+    ])
+    assert os.path.exists("downloaded_comics/X-Factor-1986") == 1
+    assert os.path.isfile("downloaded_comics/X-Factor-1986/12.pdf")
